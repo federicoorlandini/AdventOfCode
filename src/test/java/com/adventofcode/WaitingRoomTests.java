67f2527a1e3d868b8bab632aa6916d1dd446df04
Federico Orlandini <federico.orlandini@cxcompany.com>
2022-12-11 00:10:39 +0100

    dding more unit tests for the generated layout

--- src/test/java/com/adventofcode/WaitingRoomTests.java
+++ src/test/java/com/adventofcode/WaitingRoomTests.java
@@ -4,9 +4,257 @@ import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 public class WaitingRoomTests {
     @Test
-    public void next_shouldGenerateTheCorrectNextLayout() throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Not implemented");
+    public void next_shouldGenerateTheCorrectNextLayoutStep1() {
+        var layout = new String[] {
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL"
+        };
+
+        var expectedLayout = new String[] {
+            "#.##.##.##",
+            "#######.##",
+            "#.#.#..#..",
+            "####.##.##",
+            "#.##.##.##",
+            "#.#####.##",
+            "..#.#.....",
+            "##########",
+            "#.######.#",
+            "#.#####.##"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+
+    /*
+     * Assert that the two layouts are the same
+     */
+    private void assertLayouts(String[] layout, String[] expectedLayout) {
+        // Check the number of rows
+        Assertions.assertEquals(layout.length, expectedLayout.length, "The layout has a wrong number of rows");
+        for (var rowIndex: IntStream.range(0, layout.length).toArray()) {
+            // For each row, check the number of columns
+            Assertions.assertEquals(layout[rowIndex].length(), expectedLayout[rowIndex].length());
+            Assertions.assertTrue(layout.equals(expectedLayout), String.format("The row {0} doesn't contain the expected values", rowIndex));
+        }
+    }
+
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep2() {
+        var layout = new String[] {
+                "#.##.##.##",
+                "#######.##",
+                "#.#.#..#..",
+                "####.##.##",
+                "#.##.##.##",
+                "#.#####.##",
+                "..#.#.....",
+                "##########",
+                "#.######.#",
+                "#.#####.##"
+        };
+
+        var expectedLayout = new String[] {
+            "#.LL.LL.L#",
+            "#LLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLLL.L",
+            "#.LLLLL.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep3() {
+        var layout = new String[] {
+            "#.LL.LL.L#",
+            "#LLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLLL.L",
+            "#.LLLLL.L#"
+        };
+
+        var expectedLayout = new String[] {
+            "#.L#.##.L#",
+            "#L#####.LL",
+            "L.#.#..#..",
+            "##L#.##.##",
+            "#.##.#L.##",
+            "#.#####.#L",
+            "..#.#.....",
+            "LLL####LL#",
+            "#.L#####.L",
+            "#.L####.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep4() {
+        var layout = new String[] {
+            "#.L#.##.L#",
+            "#L#####.LL",
+            "L.#.#..#..",
+            "##L#.##.##",
+            "#.##.#L.##",
+            "#.#####.#L",
+            "..#.#.....",
+            "LLL####LL#",
+            "#.L#####.L",
+            "#.L####.L#"
+        };
+
+        var expectedLayout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##LL.LL.L#",
+            "L.LL.LL.L#",
+            "#.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep5() {
+        var layout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##LL.LL.L#",
+            "L.LL.LL.L#",
+            "#.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        var expectedLayout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.#L.L#",
+            "#.L####.LL",
+            "..#.#.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep6() {
+        var layout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.#L.L#",
+            "#.L####.LL",
+            "..#.#.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        var expectedLayout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.LL.L#",
+            "#.LLLL#.LL",
+            "..#.L.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
+    }
+
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayoutStep7() {
+        var layout = new String[] {
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.LL.L#",
+            "#.LLLL#.LL",
+            "..#.L.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#"
+        };
+
+        // The same initial layout
+        var expectedLayout = new String[] {
+                "#.L#.L#.L#",
+                "#LLLLLL.LL",
+                "L.L.L..#..",
+                "##L#.#L.L#",
+                "L.L#.LL.L#",
+                "#.LLLL#.LL",
+                "..#.L.....",
+                "LLL###LLL#",
+                "#.LLLLL#.L",
+                "#.L#LL#.L#"
+        };
+
+        var waitingArea = new WaitingArea(layout);
+        layout = waitingArea.next();
+
+        assertLayouts(layout, expectedLayout);
     }
 }
