5d9fc31e1e9e4849e1f1cf8d578d52d05ed80882
Federico Orlandini <federico.orlandini@cxcompany.com>
2022-12-10 20:23:01 +0100

    Including the code in a Maven project

---
+++ src/test/java/com/adventofcode/WaitingRoomTests.java
@@ -0,0 +1,12 @@
+package com.adventofcode;
+
+import jdk.jshell.spi.ExecutionControl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WaitingRoomTests {
+    @Test
+    public void next_shouldGenerateTheCorrectNextLayout() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not implemented");
+    }
+}
