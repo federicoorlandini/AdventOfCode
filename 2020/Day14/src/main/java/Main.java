162ae9e7967e3a6434b5fe42788a8153ab45088a
Federico Orlandini <federico.orlandini@cxcompany.com>
2022-12-31 18:09:53 +0100

    Start implementing the Day 14 part 2

--- 2020/Day14/src/main/java/Main.java
+++ 2020/Day14/src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        /*------------ PART 1 -------------
+        // Read the data
+        ArrayList<String> lines = readInpuData("input_part1.txt");
+
+        // Process the data
+        var cpu = new Cpu();
+        cpu.process(lines.toArray(String[]::new));
+
+        System.out.println(String.format("Result: %s", cpu.sumMemory()));
+        ------ END PART 1 ------*/
+
+        //----------- PART 2 -----------------
+        var input = readInpuData("input_part2.txt");
+
+    }
+
+    private static ArrayList<String> readInpuData(String filename) throws IOException {
+        var classLoader = ClassLoader.getSystemClassLoader();
+        var inputStream = classLoader.getResourceAsStream(filename);
+        var inputStreamReader = new InputStreamReader(inputStream);
+        var bufferedReader = new BufferedReader(inputStreamReader);
+        ArrayList<String> lines = new ArrayList<String>();
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
     }
 }
