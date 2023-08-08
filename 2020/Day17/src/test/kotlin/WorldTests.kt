4cf6dec1026573b2772444007d21f40456152401
Federico Orlandini <federico.orlandini@gmail>
2023-08-08 23:28:41 +0200

    Adding test cases for the World class - the code doesn't compile here

--- 2020/Day17/src/test/kotlin/WorldTests.kt
+++ 2020/Day17/src/test/kotlin/WorldTests.kt
@@ -2,7 +2,43 @@ import kotlin.test.Test
 
 class WorldTests {
     @Test
-    fun test1() {
+    fun scenario1Test() {
+        // This scenario is the first iteration in the example scenario
+        // that explain how the logic is implemented (see https://adventofcode.com/2020/day/17)
+
+        // Before any cycles:
+        // z=0
+        // .#.
+        // ..#
+        // ###
+        //
+        // After 1 cycle:
+        // z=-1
+        // #..
+        // ..#
+        // .#.
+        //
+        // z=0
+        // #.#
+        // .##
+        // .#.
+        //
+        // z=1
+        // #..
+        // ..#
+        // .#.
+        val initialStatus = listOf(
+            ".#.",
+            "..#",
+            "###"
+        )
+        val world = World(initialStatus)
+
+        world.iteration()
+
+        val layer0 = world.layers[0]
+
+
         throw NotImplementedError()
     }
 }
